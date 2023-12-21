20f1454b1c465df2ab51f65bdc9bb2abd0868100
ElenaKuklina <cuklina.lena@gmail.com>
2023-12-21 21:47:48 +0300

    Add Page Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,34 @@
 import React from "react";
 import classes from "./Dialogs.module.css";
-export const Dialogs =()=>{
+
+export const Dialogs = () => {
     return (
-        <div className={classes.content}>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog + ' ' + classes.active}>
+                    Dimych
+                </div>
+                <div className={classes.dialog}>
+                    Andrey
+                </div>
+                <div className={classes.dialog}>
+                    Sveta
+                </div>
+                <div className={classes.dialog}>
+                    Saha
+                </div>
+                <div className={classes.dialog}>
+                    Victor
+                </div>
+                <div className={classes.dialog}>
+                    Valera
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.dialog}>Hi</div>
+                <div className={classes.dialog}>How is your it-incubator?</div>
+                <div className={classes.dialog}>Yo</div>
+            </div>
         </div>
     )
 }
