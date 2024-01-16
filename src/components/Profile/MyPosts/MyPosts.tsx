d430a12383b2a61f5e9c51e23ea22668df3b5929
ElenaKuklina <cuklina.lena@gmail.com>
2024-01-16 23:43:47 +0300

    24_Lessons: UI, BLL, putting the data aside. Code refactoring

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,6 +3,12 @@ import classes from "./MyPosts.module.css";
 import {Post} from "./Post/Post";
 
 export const MyPosts: FC = () => {
+
+    let postsData = [
+        {id: 1, message: 'Hi, how are you?', likesCount: 53},
+        {id: 2, message: "It's my posts", likesCount: 37},
+    ]
+
     return (
         <div className={classes.postBlock}>
             <h3>My Posts</h3>
@@ -16,8 +22,8 @@ export const MyPosts: FC = () => {
                 New Post
             </div>
             <div className={classes.posts}>
-            <Post message={'Hi, how are you?'} likesCount={53}/>
-            <Post message={"It's my posts"} likesCount={37}/>
+            <Post message={postsData[0].message} likesCount={postsData[0].likesCount}/>
+            <Post message={postsData[1].message} likesCount={postsData[1].likesCount}/>
             </div>
         </div>
     )
