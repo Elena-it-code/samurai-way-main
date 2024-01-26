62ed2017f4000bfe217773f4c42128dfad96cd3c
ElenaKuklina <cuklina.lena@gmail.com>
2024-01-26 21:00:49 +0300

    Code refactoring. MyPosts. using map()

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,12 +2,20 @@ import React, {FC} from "react"
 import classes from "./MyPosts.module.css";
 import {Post} from "./Post/Post";
 
+// type postDataTypeProps = {
+//     id: number
+//     message: string
+//     likesCount: number
+// }
+
 export const MyPosts: FC = () => {
 
     let postsData = [
         {id: 1, message: 'Hi, how are you?', likesCount: 53},
         {id: 2, message: "It's my posts", likesCount: 37},
     ]
+
+    let postsElement = postsData.map(p => <Post key={p.id} message={p.message} likesCount={p.likesCount}/>)
 
     return (
         <div className={classes.postBlock}>
@@ -22,8 +30,9 @@ export const MyPosts: FC = () => {
                 New Post
             </div>
             <div className={classes.posts}>
-            <Post message={postsData[0].message} likesCount={postsData[0].likesCount}/>
-            <Post message={postsData[1].message} likesCount={postsData[1].likesCount}/>
+                {postsElement}
+                {/*<Post message={postsData[0].message} likesCount={postsData[0].likesCount}/>
+            <Post message={postsData[1].message} likesCount={postsData[1].likesCount}/>*/}
             </div>
         </div>
     )
