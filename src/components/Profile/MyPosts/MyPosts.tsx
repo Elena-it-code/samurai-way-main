bcc821d8afb419fe8b9f7c14e19362269e230410
ElenaKuklina <cuklina.lena@gmail.com>
2024-02-18 21:57:18 +0300

    31_Lessons: onClick, ref, VirtualDOM

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,25 +1,33 @@
 import classes from "./MyPosts.module.css";
 import {Post} from "./Post/Post";
-import {RootStateType, state} from "../../../redux/state";
+import React from "react";
+import {RootStateType} from "../../../redux/state";
 
 
-export const MyPosts= (props: RootStateType) => {
+export const MyPosts= (props:RootStateType ) => {
 
     /*let postsData = [
         {id: 1, message: 'Hi, how are you?', likesCount: 53},
         {id: 2, message: "It's my posts", likesCount: 37},
     ]*/
 
+    let newPostsElement = React.createRef<HTMLTextAreaElement>();
+    const addPostHandler = ()=> {
+        let text = newPostsElement.current?.value
+        alert(text)
+    }
+
     let postsElement = props.state.profilePage.postsData.map(p => <Post key={p.id} message={p.message} likesCount={p.likesCount}/>)
+
 
     return (
         <div className={classes.postBlock}>
             <h3>My Posts</h3>
             <div>
-                <textarea className={classes.textarea} placeholder={'your news'}/>
+                <textarea ref={newPostsElement} className={classes.textarea} placeholder={'your news'}></textarea>
             </div>
             <div>
-                <button className={classes.sendPost}>Send</button>
+                <button onClick={ addPostHandler } className={classes.sendPost}>Add post</button>
             </div>
             <div>
                 New Post
